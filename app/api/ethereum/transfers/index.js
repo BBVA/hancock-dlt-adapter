b7ddca3120808eed103e466e24637e668263f41d
Juan Francisco Monge <juanfrancisco.monge@bbva.com>
2018-05-16 13:08:39 +0200

    new feature for rawtx of ether transfer

--- app/api/ethereum/transfers/index.js
+++ app/api/ethereum/transfers/index.js
@@ -5,12 +5,13 @@ var validate = require('express-jsonschema').validate;
 var router = express.Router();
 
 var transaction = require('./transfersController');
-var TransactionModel = require('../../../../raml/schemas/requests/ethereum/transfers/transaction.json');
+// var TransactionModel = require('../../../../raml/schemas/requests/ethereum/transfers/transaction.json');
 
 //router.route('/:address').post(validate({ body: TransactionModel}), transaction.sendTransaction);
 
-router.get('/gasprice', transaction.gasPrice);
-router.get('/:txhash', transaction.getTransaction);
+// router.get('/gasprice', transaction.gasPrice);
+// router.get('/:txhash', transaction.getTransaction);
+router.post('/', transaction.sendTransaction);
 //router.get('/block/:block', transaction.getTransactionFromBlock(request, reply, next));
 //router.get('/:txhash/receipt', transaction.getTransactionReceipt(request, reply, next));
 //router.post('/:address', transaction.sendTransaction);
