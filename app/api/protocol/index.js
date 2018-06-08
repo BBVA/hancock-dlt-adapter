a45eb8fef15c66dd70ed869c3b825412b1e972b7
Juan Francisco Monge <juanfrancisco.monge@bbva.com>
2018-06-08 09:21:49 +0200

    protocol v1

---
+++ app/api/protocol/index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const express = require('express');
+const validate = require('express-jsonschema').validate;
+const router = express.Router();
+
+const ProtocolEncodeModel = require(`${CONF.raml}/schemas/requests/protocol/encode.json`);
+const protocolController = require('./protocolController');
+
+router.route('/encode').put(validate({body: ProtocolEncodeModel}), protocolController.encode);
+router.route('/qrEncode').put(validate({body: ProtocolEncodeModel}), protocolController.qrEncode);
+router.route('/decode/:dataEncode').get(protocolController.decode);
+
+//router.get('/:name', contract.contractInfo(request, reply, next));
+
+// Healthcheck
+router.get('/', (req, res) => {
+  res.json({
+    status: 'OK',
+    app: 'Protocol'
+  });
+});
+
+module.exports = router;
