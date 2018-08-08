006c4afc47c8be4c4ef65d83097b43e50171d56a
Angel Manzano <angel.manzano.contractor@bbva.com>
2018-08-08 21:25:43 +0200

    added delete token + tests

--- src/domain/ethereum/token/__tests__/delete.spec.ts
+++ src/domain/ethereum/token/__tests__/delete.spec.ts
@@ -1,10 +1,15 @@
 import 'jest';
+import * as db from '../../../../db/ethereum';
+import { hancockDbError } from '../../../../models/error';
+import { TokenNames } from '../../../../models/ethereum';
 import { error } from '../../../../utils/error';
+import { hancockContractNotFoundError } from '../../models/error';
 import * as scDeleteDomain from '../../smartContract/delete';
 import * as tokenDeleteDomain from '../delete';
 import { hancockContractTokenDeleteError } from '../models/error';
 
 jest.mock('../../smartContract/delete');
+jest.mock('../../../../db/ethereum');
 jest.mock('../../../../utils/logger');
 jest.mock('../../../../utils/error');
 
@@ -13,15 +18,23 @@ describe('tokenDeleteDomain', () => {
   describe('::tokenDeleteByQuery', () => {
 
     const scDeleteMock: jest.Mock = (scDeleteDomain.deleteByQuery as any);
+    const getSmartContractByAddressOrAliasMock: jest.Mock = (db.getSmartContractByAddressOrAlias as any);
     const addressOrAlias: string = 'mockedAddressOrAlias';
+
+    const successContractModelMock = {
+      abiName: TokenNames.ERC20,
+    };
 
     beforeEach(() => {
 
       scDeleteMock.mockReset();
+      getSmartContractByAddressOrAliasMock.mockReset();
 
     });
 
     it('should call the smartContract.deleteByQuery method and delete the token but not the abi', async () => {
+
+      getSmartContractByAddressOrAliasMock.mockResolvedValueOnce(successContractModelMock);
 
       const result = await tokenDeleteDomain.tokenDeleteByQuery(addressOrAlias);
 
@@ -30,7 +43,9 @@ describe('tokenDeleteDomain', () => {
 
     });
 
-    it('should throw an exception if there are problems deleteing the token', async () => {
+    it('should throw an exception if there are problems deleting the token', async () => {
+
+      getSmartContractByAddressOrAliasMock.mockResolvedValueOnce(successContractModelMock);
 
       const throwedError: Error = new Error('Boom!');
       scDeleteMock.mockRejectedValueOnce(throwedError);
@@ -45,6 +60,45 @@ describe('tokenDeleteDomain', () => {
         expect(scDeleteMock).toHaveBeenCalled();
         expect(error).toHaveBeenCalledWith(hancockContractTokenDeleteError, throwedError);
         expect(e).toEqual(hancockContractTokenDeleteError);
+
+      }
+
+    });
+
+    it('should throw an exception if there are problems deleting the token (null contractModel)', async () => {
+
+      getSmartContractByAddressOrAliasMock.mockResolvedValueOnce(null);
+
+      try {
+
+        await tokenDeleteDomain.tokenDeleteByQuery(addressOrAlias);
+        fail('It should fail');
+
+      } catch (e) {
+
+        expect(scDeleteMock).not.toHaveBeenCalled();
+        expect(error).toHaveBeenCalledWith(hancockContractNotFoundError);
+        expect(e).toEqual(hancockContractNotFoundError);
+
+      }
+
+    });
+
+    it('should throw an exception if there are problems deleting the token (error retrieving contractModel)', async () => {
+
+      const throwedError: Error = new Error('Boom!');
+      getSmartContractByAddressOrAliasMock.mockRejectedValueOnce(throwedError);
+
+      try {
+
+        await tokenDeleteDomain.tokenDeleteByQuery(addressOrAlias);
+        fail('It should fail');
+
+      } catch (e) {
+
+        expect(scDeleteMock).not.toHaveBeenCalled();
+        expect(error).toHaveBeenCalledWith(hancockDbError, throwedError);
+        expect(e).toEqual(hancockDbError);
 
       }
 
--- src/domain/ethereum/token/delete.ts
+++ src/domain/ethereum/token/delete.ts
@@ -1,19 +1,44 @@
+import * as db from '../../../db/ethereum';
 import * as scDomainDelete from '../../../domain/ethereum/smartContract/delete';
+import { hancockDbError } from '../../../models/error';
+import { IEthereumContractDbModel } from '../../../models/ethereum';
+import { TokenNames } from '../../../models/ethereum/token';
 import { error } from '../../../utils/error';
 import logger from '../../../utils/logger';
+import { hancockContractNotFoundError } from '../models/error';
 import { hancockContractTokenDeleteError } from './models/error';
 
 export async function tokenDeleteByQuery(addressOrAlias: string): Promise<void> {
 
   logger.info(`De-registering token contract by query: ${addressOrAlias}`);
 
+  let contractDbModel: IEthereumContractDbModel | null;
+
   try {
 
-    await scDomainDelete.deleteByQuery(addressOrAlias, false);
+    contractDbModel = await db.getSmartContractByAddressOrAlias(addressOrAlias);
 
   } catch (e) {
 
-    throw error(hancockContractTokenDeleteError, e);
+    throw error(hancockDbError, e);
+
+  }
+
+  if (contractDbModel && contractDbModel.abiName === TokenNames.ERC20) {
+
+    try {
+
+      await scDomainDelete.deleteByQuery(addressOrAlias, false);
+
+    } catch (e) {
+
+      throw error(hancockContractTokenDeleteError, e);
+
+    }
+
+  } else {
+
+    throw error(hancockContractNotFoundError);
 
   }
 
