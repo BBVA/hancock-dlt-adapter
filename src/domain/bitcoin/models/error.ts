4939ee88c04f8d499e40bc4da57e2101cbee507d
Juan Francisco Monge <juanfrancisco.monge@bbva.com>
2018-12-17 16:09:42 +0100

    Clean BTC

--- src/domain/bitcoin/models/error.ts
+++
@@ -1,5 +0,0 @@
-import { HancockError } from '../../../models/error';
-
-export const hancockContractNotFoundError = new HancockError('40401', 404, 'Smart Contract - Not Found');
-export const hancockBitcoinBalanceError = new HancockError('50023', 500, 'Bitcoin - Balance Error');
-export const hancockBitcoinTransferError = new HancockError('50024', 500, 'Bitcoin - Transfer Error');
--- src/utils/bitcoin/__mocks__/utils.ts
+++
@@ -1,1 +0,0 @@
-export const getScQueryByAddressOrAlias = jest.fn();
